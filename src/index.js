ecb0f7ebe656529313c7a4394b4a5be5aa9e1536
Derek Lim <dereklim3006@gmail.com>
2025-05-17 21:39:57 -0400

    Write function that fetches weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './styles.css'
+
+async function getWeatherData(location) {
+  const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=JGM7G4V9AVASVNWUXBBZVPUPZ`)
+  const weatherData = await response.json()
+  const address = weatherData.address
+  const conditions = weatherData.currentConditions.conditions
+  const temp = weatherData.currentConditions.temp
+  const windspeed = weatherData.currentConditions.windspeed
+   
+  return { address, conditions, temp, windspeed }
+}
