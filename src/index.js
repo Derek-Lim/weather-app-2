c7253b5ff75dfca6d9db1e7b20fba4a5c2632f4e
Derek Lim <dereklim3006@gmail.com>
2025-05-18 12:33:55 -0400

    Allow user to search

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import './styles.css'
 
+const form = document.querySelector('form')
+form.addEventListener('submit', handleSubmit)
+
+function getLocation() {
+  const input = form.querySelector('input')
+  return input.value
+}
+
+async function handleSubmit(event) {
+  event.preventDefault()
+  const weatherData = await getWeatherData(getLocation())
+}
+
 async function getWeatherData(location) {
   const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=JGM7G4V9AVASVNWUXBBZVPUPZ`)
   const weatherData = await response.json()
